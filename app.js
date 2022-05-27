4cb18fb5aa523e9da74710be0dafb4628fd1b187
mohammad-fatemi <mohammad.ftm.021@gmail.com>
2022-05-27 23:54:17 +0430

    html and css is done and all elements are selected in js

---
+++ app.js
@@ -0,0 +1,17 @@
+const $=document;
+const typeOfConvert=$.querySelector("#convertType");
+const numberBox=$.querySelector("numberBox");
+const result=$.querySelector(".resulte");
+const convertBtn=$.getElementById("convertBtn");
+const resetBtn=$.getElementById("resetBtn");
+const changeBtn=$.getElementById("changeBtn");
+
+
+//when change button clicked
+
+
+
+//when reset button clicked
+
+
+//when convert button clecked
